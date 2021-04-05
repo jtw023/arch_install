2d1922fc46e969c9bf4c37c88ba9dc02eabaa75b
jtw023 <59935328+jtw023@users.noreply.github.com>
2021-04-04 22:43:28 -0700

    feat: separated large file into smaller files

--- Install.sh
+++ Install.sh
@@ -79,6 +79,7 @@ pulseaudio-bluetooth
 python3
 qemu
 qtile
+qutebrowser
 ranger
 redshift
 reflector
@@ -110,84 +111,6 @@ for name in "${list[@]}" ; do
 	func_install $name
 done
 
-###############################################################################
-
 echo "################################################################"
-echo "Cloning from git."
-echo "################################################################"
-echo "----------------------------------------------------------------"
-echo "cd to /usr/share/backgrounds/"
-echo "----------------------------------------------------------------"
-cd /usr/share/backgrounds/
-git clone https://gitlab.com/dwt1/wallpapers.git
-echo "----------------------------------------------------------------"
-echo "cd to /usr/share/fonts/"
-echo "----------------------------------------------------------------"
-cd /usr/share/fonts/
-git clone https://github.com/jtw023/fonts.git
-echo "----------------------------------------------------------------"
-echo "cd to /opt/"
-echo "----------------------------------------------------------------"
-cd /opt/
-git clone https://aur.archlinux.org/yay.git
-echo "----------------------------------------------------------------"
-echo "cd to /home/jordan/"
-echo "----------------------------------------------------------------"
-cd /home/jordan/
-git clone https://github.com/jtw023/.config.git
-git clone --depth 1 https://github.com/junegunn/fzf.git
-echo "----------------------------------------------------------------"
-echo "Install zsh fuzzy finder"
-echo "----------------------------------------------------------------"
-/home/jordan/.fzf/install --all
-mkdir -v /usr/share/zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/zsh/plugins/zsh-autosuggestions/
-mkdir -v /home/jordan/.local/bin
-git clone https://github.com/holman/spark.git /home/jordan/.local/bin/
-chmod -v +x /home/jordan/.local/bin/spark.sh
-git clone https://github.com/jtw023/Random-Scripts.git
-chmod -v +x /home/jordan/Random-Scripts/*
-curl -fLo /home/jordan/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -c ':PlugInstall | quit'
-
-echo "################################################################"
-echo "Moving packages"
-echo "################################################################"
-if [ -d "/usr/share/zsh/themes" ]; then 
-	cp -v /home/jordan/.config/zsh/bira.zsh-theme /usr/share/zsh/themes/
-elif [ -d "/usr/share/zsh" ]; then
-	mkdir -v /usr/share/zsh/themes
-	cp -v /home/jordan/.config/zsh/bira.zsh-theme /usr/share/zsh/themes/
-elif [ -d "/usr/share" ]; then
-	mkdir -v /usr/share/zsh
-	mkdir -v /usr/share/zsh/themes
-	cp -v /home/jordan/.config/zsh/bira.zsh-theme /usr/share/zsh/themes/
-else 
-	mkdir -v /usr/share
-	mkdir -v /usr/share/zsh
-	mkdir -v /usr/share/zsh/themes
-	cp -v /home/jordan/.config/zsh/bira.zsh-theme /usr/share/zsh/themes/
-fi
-mkdir -v /opt/shell-color-scripts
-cp -v /home/jordan/.config/Xresources/.Xresources /home/jordan/
-cp -v /home/jordan/.config/xinit/.xinitrc /home/jordan/
-cp -v /home/jordan/.config/Xauthority/.Xauthority /home/jordan/
-cp -vr /home/jordan/.config/colorscripts/ /opt/shell-color-scripts/
-cp -v /home/jordan/.config/vim/.vimrc.root /root/.vimrc
-cp -v /home/jordan/.config/zsh/.zshrc.root /root/.zshrc
-cp -v /home/jordan/.config/zsh/.zshrc /home/jordan/
-cp -v /home/jordan/.config/vim/.vimrc /home/jordan/
-
-
-echo "################################################################"
-echo "Enabling yay."
-echo "################################################################"
-echo "----------------------------------------------------------------"
-echo "cd to /opt/"
-echo "----------------------------------------------------------------"
-cd /opt/
-chown -Rv jordan:users ./yay
-
-echo "################################################################"
-echo "Finished running. Please modify /etc/lightdm/lightdm.conf and set 'greeter-session' in the [Seat:*] group. Then run 'systemctl enable sshd; systemctl enable NetworkManager; systemctl enable lightdm; systemctl enable bluetooth"
+echo "Finished installing. Please modify /etc/lightdm/lightdm.conf and set 'greeter-session' in the [Seat:*] group. Then run 'systemctl enable sshd; systemctl enable NetworkManager; systemctl enable lightdm; systemctl enable bluetooth"
 echo "################################################################"
