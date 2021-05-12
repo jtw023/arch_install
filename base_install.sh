759185a3e609b64208e005bca5ab0bfbd5ba3356
jtw023 <r3stock3r@tutanota.com>
2021-05-11 20:45:19 -0700

    fixed errors

--- base_install.sh
+++ base_install.sh
@@ -242,7 +242,7 @@ passwd $user
 
 echo "################## Installing grub ##################"
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 
 # echo "################## Copying grub locale ##################"
 
