6c6fb8b01a588c69b909541829fe62cbddbd958a
jtw023 <r3stock3r@tutanota.com>
2021-05-12 00:29:04 -0700

    fixed colors

--- paru_and_lunarvim_setup.sh
+++ paru_and_lunarvim_setup.sh
@@ -13,7 +13,7 @@ func_install() {
 
 	else
 
-		paru -S --noremovemake $1
+		paru -S --skipreview $1
 
     fi
 
@@ -152,7 +152,7 @@ if [[ $UID -ne 0 ]]; then
 	echo "${BLUE}Sleeping for 30 seconds while you read this${NC}."
 	sleep 30
 
-    echo -e "${GREEN}Finished. Be sure to sync any backed up files and make sure the correct version of lunarvim is installed.\nYou can also change the wifi that gets logged into on boot, the redshift amount, and the xrandr command that runs by editing "$HOME"/.config/qtile/scripts/autostart.sh\n\nPlease open nvim and run ':PackerCompile', ':PackerInstall', ':LspInstall efm', and ':LspInstall python'\n\nRemember: you can only run 'nvim', 'pacman -Syu', and 'pacman -Rns' as doas. Sudo is aliased to doas in "$HOME"/.config/zsh/.zshrc. For all other commands please switch to the root user using 'su'${NC}."
+    echo -e ${GREEN}"Finished. Be sure to sync any backed up files and make sure the correct version of lunarvim is installed.\nYou can also change the wifi that gets logged into on boot, the redshift amount, and the xrandr command that runs by editing "$HOME"/.config/qtile/scripts/autostart.sh\n\nPlease open nvim and run ':PackerCompile', ':PackerInstall', ':LspInstall efm', and ':LspInstall python'\n\nRemember: you can only run 'nvim', 'pacman -Syu', and 'pacman -Rns' as doas. Sudo is aliased to doas in "$HOME"/.config/zsh/.zshrc. For all other commands please switch to the root user using 'su'${NC}."
 
 else
 
