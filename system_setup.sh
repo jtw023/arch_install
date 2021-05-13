037c9c4f034ad2478866208bdad3e37b51c74fc7
jtw023 <r3stock3r@tutanota.com>
2021-05-13 14:22:14 -0700

    fixed one line in doas config file section

--- system_setup.sh
+++ system_setup.sh
@@ -15,7 +15,7 @@ else
 	touch /etc/doas.conf
 	echo 'permit nopass '$USER' as root' > /etc/doas.conf
 	cat /etc/doas.conf
-	echo -e "${BLUE}If the above line is not 'permit <yourusername> as root' then please come back to modify /etc/doas.conf${NC}"
+	echo -e "${BLUE}If the above line is not 'permit nopass <yourusername> as root' then please come back to modify /etc/doas.conf${NC}"
 	echo -e "${BLUE}Sleeping for 10 seconds while you read this${NC}."
 	sleep 10
 
